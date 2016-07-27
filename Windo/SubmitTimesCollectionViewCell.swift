6a9f0090feccde29ac34768f391e10cf1318ff26
Joey Nelson <joey.nelson@glassdoor.com>
2016-07-27 08:15:44 -0700

    created the basic structure of the new new SubmitTimes (i.e. expanding time cells! yahoo for :15, :30, :45 time intervals!)

---
+++ Windo/SubmitTimesCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  SubmitTimesCollectionViewCell.swift
+//  Windo
+//
+//  Created by Joey Nelson on 7/27/16.
+//  Copyright © 2016 NelsonJE. All rights reserved.
+//
+
+import Foundation
---
+++ Windo/SubmitTimesViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SubmitTimesViewController.swift
+//  Windo
+//
+//  Created by Joey Nelson on 7/27/16.
+//  Copyright © 2016 NelsonJE. All rights reserved.
+//
+
+import Foundation
